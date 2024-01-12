011ad8bb4aef47246382a255e772b2622fe1029e
Ceejayski <sirchijioke1@gmail.com>
2024-01-12 20:24:34 +0100

    setup app for render deploy

---
+++ render_build.sh
@@ -0,0 +1,10 @@
+# exit on error
+set -o errexit
+
+bundle install
+
+bundle install
+./bin/rails assets:precompile
+./bin/rails assets:clean
+bundle exec rails db:migrate
+bundle exec rails db:seed
