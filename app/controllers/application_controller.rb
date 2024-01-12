4d564e73d9d9447272238eef9a4449a41c15966c
Ceejayski <sirchijioke1@gmail.com>
2024-01-12 20:57:05 +0100

    Fix session issue in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user_ip_address
 
   private
-
     def set_current_user_ip_address
-      Current.ip_address = request.ip
+      session[:ip_address] ||= SecureRandom.urlsafe_base64(16)
+      # Current.ip_address = request.ip # This will not work in production
+      Current.ip_address = session[:ip_address]
     end
 end
